5fb389d885cde8556e8ba7e307936b9c2dbc2a8e
encodedBicoding <dominicisioma000@gmail.com>
2024-02-19 08:13:04 +0100

    updates fix cron code

--- server/cron.js
+++ server/cron.js
@@ -11,7 +11,7 @@ class Cron {
     }
 
     async preventSleep() {
-        cron.schedule('10 * * * *', async () => {
+        cron.schedule('*/10 * * * *', async () => {
             await fetch(process.env.SERVER_API,{
                 method: 'GET',
             })
